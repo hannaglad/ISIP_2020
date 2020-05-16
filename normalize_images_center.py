1b0073a189b4595a59715e93d22b770a6ec665e3
hannaglad <62237905+hannaglad@users.noreply.github.com>
2020-05-16 16:25:58 +0200

    added find electrodes (only works on some images)

--- normalize_images_center.py
+++ normalize_images_center.py
@@ -56,3 +56,33 @@ img = cv2.imread(testImage,0)
 img = cv2.normalize(img,  None, 0, 255, cv2.NORM_MINMAX)
 img = cv2.medianBlur(img,13)
 find_center(img)
+
+
+
+def findElectrodes(pre, post):
+    # Find contours in post operative image
+    post = cv2.imread(post,0)
+    pre = cv2.imread(pre, 0)
+
+    post = cv2.normalize(post, None, 0, 255, cv2.NORM_MINMAX)
+    post = cv2.medianBlur(post, 13)
+    ret2,th2 = cv2.threshold(post,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    th2 = th2-255
+    display(th2)
+
+    kernel = np.ones((3,3), np.uint8)
+    erosion = cv2.erode(th2, kernel, iterations=11)
+    dilation = cv2.dilate(erosion, kernel, iterations=5)
+    im, cnts, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    # Find spiral center in pre opertive image
+    pre = find_center(pre)
+    post = cv2.cvtColor(post, cv2.COLOR_GRAY2BGR)
+    #compute center of contours in post operative image
+    for c in cnts:
+        M = cv2.moments(c)
+        cX = int(M["m10"]/M["m00"])
+        cY = int(M["m01"]/M["m00"])
+        cv2.drawContours(post, c, -1, (0,255,0),1)
+        cv2.circle(post, (cX, cY),3, (0,255,0), -1)
+    display(post)
