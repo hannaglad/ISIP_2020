800165a78a8fd578036d8a1e53744fbe9cb11627
hannaglad <62237905+hannaglad@users.noreply.github.com>
2020-05-17 15:19:27 +0200

    updated find electrodes
    used a mask to reduce the searchspace

--- normalize_images_center.py
+++ normalize_images_center.py
@@ -58,20 +58,44 @@ img = cv2.medianBlur(img,13)
 find_center(img)
 
 
+def searchSpace(pre, post):
+    pre_color = cv2.cvtColor(pre,cv2.COLOR_GRAY2BGR)
+    ret2,th2 = cv2.threshold(pre,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    th2 = 255-th2
+    circles = cv2.HoughCircles(th2,cv2.HOUGH_GRADIENT,1,1000,
+                            param1=200,param2=15,minRadius=50,maxRadius=230)
+    circles = np.uint16(np.around(circles))
+    x,y,r = circles[0,:][0]
+    rows, cols = pre.shape
 
-def findElectrodes(pre, post):
+    mask = np.ones((post.shape[0], post.shape[1]))
+    mean_value = np.mean(post)
+
+    for i in range(cols):
+        for j in range(rows):
+            if math.hypot(i-x, j-y) > r+150:
+                mask[j,i] = mean_value
+            else :
+                mask[j,i] = post[j,i]
+
+
+    return mask
+
+def findElectrodes(pre, post_reduced, post):
     # Find contours in post operative image
-    post = cv2.imread(post,0)
-    pre = cv2.imread(pre, 0)
+    post_reduced = cv2.normalize(post_reduced, None, 0, 255, cv2.NORM_MINMAX)
+    #post_reduced = cv2.GaussianBlur(post_reduced, (11,11),4)
 
-    post = cv2.normalize(post, None, 0, 255, cv2.NORM_MINMAX)
-    post = cv2.medianBlur(post, 13)
-    ret2,th2 = cv2.threshold(post,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    th2 = th2-255
-    display(th2)
+    post_reduced = np.uint8(post_reduced)
+    post_reduced = cv2.bilateralFilter(post_reduced, 9, 75, 75)
+    #display(post_reduced)
+    ret2,th2 = cv2.threshold(post_reduced,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    #th2 = cv2.adaptiveThreshold(post_reduced, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21,0)
+
+    #display(th2)
 
     kernel = np.ones((3,3), np.uint8)
-    erosion = cv2.erode(th2, kernel, iterations=11)
+    erosion = cv2.erode(th2, kernel, iterations=15)
     dilation = cv2.dilate(erosion, kernel, iterations=5)
     im, cnts, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
@@ -79,10 +103,15 @@ def findElectrodes(pre, post):
     pre = find_center(pre)
     post = cv2.cvtColor(post, cv2.COLOR_GRAY2BGR)
     #compute center of contours in post operative image
+    X = []
+    Y = []
     for c in cnts:
         M = cv2.moments(c)
         cX = int(M["m10"]/M["m00"])
         cY = int(M["m01"]/M["m00"])
+        X.append(cX)
+        Y.append(cY)
         cv2.drawContours(post, c, -1, (0,255,0),1)
         cv2.circle(post, (cX, cY),3, (0,255,0), -1)
-    display(post)
+    return post
+    #return X,Y
